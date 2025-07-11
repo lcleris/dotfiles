452cfad216fa7beb896359fff16a6434095ed1ca
Loan CLERIS <lcleris@masana.care>
2025-07-11 09:55:41 +0200

    refactor(nuhsell and ghostty): remove fish and tmux
    i'm using zellij, nushell and Ghostty

--- .config/fish/conf.d/ruby.fish
+++
@@ -1,3 +0,0 @@
-set -gx RUBY_VERSION "3.3.0"
-set -gx RUBIES "$HOME/.rubies"
-set -gx PATH "$RUBIES/ruby-$RUBY_VERSION/bin" $PATH
--- .config/fish/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls
-    eza --icons --long --classify --git --header $argv
-end
