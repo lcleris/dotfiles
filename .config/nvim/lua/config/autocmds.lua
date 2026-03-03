3cda4c196160d1c032649dc5b97e80725e9dd2a0
Loan CLERIS <lcleris@masana.care>
2026-03-03 15:39:46 +0100

    fix(nvim): force the lsp eslint fix all on pre buffer
    check if conform is the correct solution for me and maybe replace it but
    lsp-config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+  callback = function()
+    local clients = vim.lsp.get_clients({ name = "eslint" })
+    if #clients > 0 then
+      vim.cmd("LspEslintFixAll")
+    end
+  end,
+})
